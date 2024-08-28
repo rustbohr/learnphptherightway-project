8d9684c391fb0d72c20f804b331d9aee6ecfd4bc
Растко Боричић <rboricic89@gmail.com>
2024-08-28 17:35:36 +0200

    FIX: empty transactions check in view

--- views/transactions.php
+++ views/transactions.php
@@ -35,47 +35,49 @@
     </tr>
     </thead>
     <tbody>
-    <?php foreach ($transactions as $transaction): ?>
-        <tr>
-            <td>
-                <?= formatDate($transaction['date']) ?>
-            </td>
-            <td>
-                <?= $transaction['checkNumber'] ?>
-            </td>
-            <td>
-                <?= $transaction['description'] ?>
-            </td>
-            <td>
-                <?php if ($transaction['amount'] < 0): ?>
-                    <span style="color: red;">
+    <?php if (!empty($transactions)): ?>
+        <?php foreach ($transactions as $transaction): ?>
+            <tr>
+                <td>
+                    <?= formatDate($transaction['date']) ?>
+                </td>
+                <td>
+                    <?= $transaction['checkNumber'] ?>
+                </td>
+                <td>
+                    <?= $transaction['description'] ?>
+                </td>
+                <td>
+                    <?php if ($transaction['amount'] < 0): ?>
+                        <span style="color: red;">
                     <?= formatDollarAmount($transaction['amount']) ?>
                     </span>
-                <?php elseif ($transaction['amount'] > 0): ?>
-                    <span style="color: green;">
+                    <?php elseif ($transaction['amount'] > 0): ?>
+                        <span style="color: green;">
                     <?= formatDollarAmount($transaction['amount']) ?>
                     </span>
-                <?php else: ?>
-                    <span>
+                    <?php else: ?>
+                        <span>
                         <?= formatDollarAmount($transaction['amount']) ?>
                     </span>
-                <?php endif ?>
-            </td>
-        </tr>
-    <?php endforeach; ?>
+                    <?php endif ?>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    <?php endif; ?>
     </tbody>
     <tfoot>
     <tr>
         <th colspan="3">Total Income:</th>
-        <td><?= formatDollarAmount($totals['totalIncome']) ?></td>
+        <td><?= formatDollarAmount($totals['totalIncome'] ?? 0) ?></td>
     </tr>
     <tr>
         <th colspan="3">Total Expense:</th>
-        <td><?= formatDollarAmount($totals['totalExpense']) ?></td>
+        <td><?= formatDollarAmount($totals['totalExpense'] ?? 0) ?></td>
     </tr>
     <tr>
         <th colspan="3">Net Total:</th>
-        <td><?= formatDollarAmount($totals['netTotal']) ?></td>
+        <td><?= formatDollarAmount($totals['netTotal'] ?? 0) ?></td>
     </tr>
     </tfoot>
 </table>
